c71543c76a6d83a1bdeffa2a8a275109b87d7cd8
Nils <nils.programming@gmail.com>
2020-02-15 14:33:38 +0100

    Added __init__ and draw methods

--- Button.py
+++ Button.py
@@ -1,6 +1,17 @@
+import pyglet
+
 class Button():
-    def __init__(x, y, witdh, height, texture=None):
-        pass
+    def __init__(self, x, y, width, height, texture=None):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        if texture == None:
+            self.texture = None
+        else:
+            self.texture = pyglet.image.load(texture)
+            self.texture = self.texture.get_region(0, 0, self.width, self.height)
 
     def draw(self):
-        pass
+        if self.texture != None:
+            self.texture.blit(self.x, self.y, 0, self.width, self.height)
