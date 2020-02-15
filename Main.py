7182ee127d2aa078a40cdfd66b0be68f6bb557c2
Nils <nils.programming@gmail.com>
2020-02-15 14:09:36 +0100

    Changed menuLoop to gameLoop

--- Main.py
+++ Main.py
@@ -2,12 +2,12 @@ import pyglet
 import Game
 
 i = 0
-def menuLoop(dt):
+def gameLoop(dt):
     global i
     i += 1
     if i%60 == 0:
         print(i//60)
 
-window = Game.Window(menuLoop)
+window = Game.Window(gameLoop)
 #pyglet.clock.schedule_interval(menuLoop, 1/60)
 pyglet.app.run()
